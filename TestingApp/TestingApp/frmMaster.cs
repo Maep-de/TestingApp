14762641f5535c7b19906a0217f48cd497a65716
sadikatanzila <57591240+sadikatanzila@users.noreply.github.com>
2021-05-26 10:38:23 +0200

    Add files via upload

---
+++ TestingApp/TestingApp/frmMaster.cs
@@ -0,0 +1,56 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace TestingApp
+{
+    public partial class frmMaster : Form
+    {
+        public frmMaster()
+        {
+            InitializeComponent();
+        }
+
+        
+
+        public void showForm(Form frm)
+        {
+            closeForm();
+
+            this.IsMdiContainer = true;
+            frm.MdiParent = this;
+            frm.Show();
+        }
+        public void closeForm()
+        {
+            foreach (Form frm in this.MdiChildren)
+            {
+                frm.Close();
+            }
+        }
+        private void distanceToolStripMenuItem_Click(object sender, EventArgs e)
+        {
+            Form frm = new frmSearchwithDistance();
+            showForm(frm);
+        }
+        private void personToolStripMenuItem_Click(object sender, EventArgs e)
+        {
+            Form frm = new frm_distanceAddress();
+            showForm(frm);
+        }
+
+        
+
+        private void distanceFromPersonToolStripMenuItem_Click(object sender, EventArgs e)
+        {
+            Form frm = new frm_persondistance();
+            showForm(frm);
+        }
+    }
+}
---
+++ TestingApp/TestingApp/frm_distanceAddress.cs
@@ -0,0 +1,56 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace TestingApp
+{
+    
+    public partial class frm_distanceAddress : Form
+    {
+        clsConsolers clsConsole = new clsConsolers();
+
+        public frm_distanceAddress()
+        {
+            InitializeComponent();
+            clsConsole.fiil_CBO(ddlAdressID);
+        }
+
+        //private void button1_Click(object sender, EventArgs e)
+        //{
+        //    // string value = ddlAdressID.SelectedItem.ToString();
+        //    label3.Text = ddlAdressID.SelectedValue.ToString();
+        //    label4.Text = ddlAdressID.SelectedItem.ToString()+"-item-"+
+        //        ddlAdressID.SelectedIndex.ToString() + "-index-" + ddlAdressID.SelectedText.ToString()
+        //        + "-text-";
+
+        //}
+
+        private void ddlAdressID_SelectedIndexChanged(object sender, EventArgs e)
+        {
+            label3.Text = ddlAdressID.SelectedValue.ToString();
+            label4.Text = ddlAdressID.SelectedItem.ToString() + "-item-" +
+                ddlAdressID.SelectedIndex.ToString() + "-index-" + ddlAdressID.SelectedText.ToString()
+                + "-text-";
+        }
+
+        private void btnSearch_Click(object sender, EventArgs e)
+        {
+            int Addressvalue = Convert.ToInt32(ddlAdressID.SelectedValue.ToString());
+            int value = Convert.ToInt32(txtValue.Text);
+
+            if (Addressvalue > 0 && value > 0)
+            {
+                clsConsole.SearchProductData(lstTeachers, Addressvalue,value);
+            }
+            
+        }
+
+        
+    }
+}
